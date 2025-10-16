17d87242074a6d95c3856cb049da957cec4d34b7
BHan2002 <bhan16@ucsc.edu>
2025-10-16 13:37:37 -0700

    Step1 Complete: Made a Canvas and Header for the project. Canvas has a boarder, shadow, and rounded edges.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,42 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+document.body.innerHTML = `  
 `;
+/*----Header Creation-------------------------------------------*/
+const header = document.createElement("h1");
+header.textContent = "Canvas";
+document.body.appendChild(header);
+/*----Canvas Creation-------------------------------------------*/
+const canvas = document.createElement("canvas");
+canvas.classList.add("canvas-screen");
+document.body.appendChild(canvas);
+/*----Canvas Drawable-------------------------------------------*/
+const ctx = canvas.getContext("2d")!;
+let isDrawing = false;
+
+canvas.addEventListener("mousedown", (e) => {
+  isDrawing = true;
+  const rect = canvas.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (!isDrawing) return;
+
+  /*--Throttle drawing to refresh rate using rAF--*/
+  requestAnimationFrame(() => {
+    const rect = canvas.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  });
+});
+
+canvas.addEventListener("mouseup", () => {
+  isDrawing = false;
+  ctx.closePath();
+});
