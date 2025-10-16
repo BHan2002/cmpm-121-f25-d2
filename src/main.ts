4b469836c30a4bc60ad8841627405ce18b0d5e99
BHan2002 <bhan16@ucsc.edu>
2025-10-16 13:52:21 -0700

    Step 2 Complete: - Implemented an event listender for the mouse allowing the user to draw on the canvas. - I also added a Clear button so that I don't have to refresh the page everytime I wanted to restart. - Moved HTML Appendings to the bottom of the main.ts for easier readability

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,17 @@ document.body.innerHTML = `
 /*----Header Creation-------------------------------------------*/
 const header = document.createElement("h1");
 header.textContent = "Canvas";
-document.body.appendChild(header);
 /*----Canvas Creation-------------------------------------------*/
 const canvas = document.createElement("canvas");
 canvas.classList.add("canvas-screen");
-document.body.appendChild(canvas);
 /*----Canvas Drawable-------------------------------------------*/
 const ctx = canvas.getContext("2d")!;
 let isDrawing = false;
 
 canvas.addEventListener("mousedown", (e) => {
   isDrawing = true;
+  ctx.lineWidth = .33;
+  ctx.lineCap = "round";
   const rect = canvas.getBoundingClientRect();
   const x = e.clientX - rect.left;
   const y = e.clientY - rect.top;
@@ -40,3 +40,20 @@ canvas.addEventListener("mouseup", () => {
   isDrawing = false;
   ctx.closePath();
 });
+/*----Clear Button-------------------------------------------*/
+const makeClearButton = (
+  canvas: HTMLCanvasElement,
+  ctx: CanvasRenderingContext2D,
+) => {
+  const button = document.createElement("button");
+  button.textContent = "Clear";
+  button.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  });
+  return button;
+};
+
+/*---Appending---*/
+document.body.appendChild(header);
+document.body.appendChild(canvas);
+document.body.appendChild(makeClearButton(canvas, ctx));
